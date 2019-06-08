db9825d3326e7eb7d86861eac3176d217228dbcf
Karo Rönty <39708710+KaroRonty@users.noreply.github.com>
2019-06-08 14:53:48 +0300

    Styling

--- TheMostImportantChart.r
+++ TheMostImportantChart.r
@@ -8,9 +8,9 @@ library(forcats)
 
 options(scipen = 1e9)
 
-data <- full_data %>% 
+data <- full_data %>%
   # Change index_real to index to get the nominal returns
-  select(dates, index_real) %>% 
+  select(dates, index_real) %>%
   na.omit()
 
 # Calculate index for each year
@@ -24,7 +24,7 @@ leaded <- as_tibble(matrix(NA,
 colnames(leaded) <- paste0("m", 1:length(index))
 
 # Loop future i month returns
-for(i in index){
+for (i in index) {
   leaded[, i] <- lead(data$index, i) / data$index
 }
 
@@ -35,25 +35,25 @@ quantiles <- cbind(sapply(leaded %>% select(m1:m600),
                           function(x) quantile(x, na.rm = T, 0.95)))
 
 # Format data
-quantiles <- quantiles %>% 
-  as.data.frame() %>% 
-  rownames_to_column(var = "key") %>% 
+quantiles <- quantiles %>%
+  as.data.frame() %>%
+  rownames_to_column(var = "key") %>%
   rename(min = V1,
-         max = V2) %>% 
+         max = V2) %>%
   mutate(key = str_remove(key, ".5%"))
 
 # Gather & attach prediction intervals
-to_plot <- leaded %>% 
-  select(m1:m600) %>% 
-  gather() %>% 
+to_plot <- leaded %>%
+  select(m1:m600) %>%
+  gather() %>%
   left_join(quantiles)
 
 # Format years, rename and plot
-to_plot %>% 
-  mutate(key = as.numeric(str_remove(key, "m"))) %>% 
+to_plot %>%
+  mutate(key = as.numeric(str_remove(key, "m"))) %>%
   rename(Year = key,
-         Multiple = value) %>% 
-  mutate(Year = as.numeric(fct_reorder(as.character(Year), sort(Year))) / 12) %>% 
+         Multiple = value) %>%
+  mutate(Year = as.numeric(fct_reorder(as.character(Year), sort(Year))) / 12) %>%
   ggplot(aes(x = Year,
              y = Multiple)) +
   stat_smooth(color = "black") +
@@ -64,4 +64,4 @@ to_plot %>%
   xlab("Years invested") +
   geom_line(aes(y = min), color = "black") +
   geom_line(aes(y = max), color = "black") +
-  theme(panel.grid.major = element_line(color="#969696",size=.25))
+  theme(panel.grid.major = element_line(color = "#969696", size = 0.25))
